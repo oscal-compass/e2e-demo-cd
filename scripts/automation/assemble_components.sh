3f9d320d1fd0d435cffb2c51bde36eb6824e7268
Lou DeGenaro <lou.degenaro@gmail.com>
2025-03-28 16:12:26 -0400

    Skip assemble when markdown folder is empty
    Signed-off-by: Lou DeGenaro <lou.degenaro@gmail.com>

--- scripts/automation/assemble_components.sh
+++ scripts/automation/assemble_components.sh
@@ -1,6 +1,10 @@
 version_tag=$1
 for d in ./component-definitions/* ; do
    compdef=$(basename "$d")
+   if ! find "md_components/$compdef" -mindepth 1 -not -name ".*" -print -quit | grep -q .; then
+      echo "Directory md_components/$compdef is empty, skipping..."
+      continue
+   fi
    echo "Assembling ${compdef}" 
    if [ "$1" != "" ]; then 
       trestle author component-assemble --name $compdef --markdown md_components/$compdef --output $compdef --version $version_tag 
