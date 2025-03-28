32c66a760482da75da479f6517ba8e77d63af074
Lou DeGenaro <lou.degenaro@gmail.com>
2025-03-28 14:01:39 -0400

    add ls for debugging...
    Signed-off-by: Lou DeGenaro <lou.degenaro@gmail.com>

--- scripts/automation/regenerate_components.sh
+++ scripts/automation/regenerate_components.sh
@@ -1,4 +1,7 @@
 trestle task csv-to-oscal-cd -c data/csv-to-oscal-cd-software.config
 trestle task csv-to-oscal-cd -c data/csv-to-oscal-cd-validation.config
 trestle author component-generate -n Ubuntu_Linux_24_04_LTS -o md_components/Ubuntu_Linux_24_04_LTS
+ls -atl md_components/Ubuntu_Linux_24_04_LTS
+ls -atl md_components/Ubuntu_Linux_24_04_LTS/Ubuntu_Linux_24.04_LTS
+ls -atl md_components/Ubuntu_Linux_24_04_LTS/Ubuntu_Linux_24.04_LTS/source_001
 trestle author component-generate -n oscap -o md_components/oscap
