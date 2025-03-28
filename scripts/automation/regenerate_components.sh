6a5ef63674c50f7dfe6e72271e7b6b45dfa4ddfb
Lou DeGenaro <lou.degenaro@gmail.com>
2025-03-28 07:47:09 -0400

    add component-generate to regenerate script
    Signed-off-by: Lou DeGenaro <lou.degenaro@gmail.com>

--- scripts/automation/regenerate_components.sh
+++ scripts/automation/regenerate_components.sh
@@ -1,2 +1,4 @@
 trestle task csv-to-oscal-cd -c data/csv-to-oscal-cd-software.config
 trestle task csv-to-oscal-cd -c data/csv-to-oscal-cd-validation.config
+trestle author component-generate -n Ubuntu_Linux_24_04_LTS -o md_components/Ubuntu_Linux_24_04_LTS
+trestle author component-generate -n oscap -o md_components/oscap
