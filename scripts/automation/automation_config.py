0b03495c2835380ae5cecb819702c175a8934851
Lou DeGenaro <lou.degenaro@gmail.com>
2025-03-28 16:29:33 -0400

    automation_config
    Signed-off-by: Lou DeGenaro <lou.degenaro@gmail.com>

---
+++ scripts/automation/automation_config.py
@@ -0,0 +1,14 @@
+import os, sys, yaml
+
+def get_value(file: str, key: str) -> str:
+    with open(file, 'r') as f:
+        data = yaml.safe_load(f)
+    return data[key]
+
+
+if __name__ == "__main__":
+    parentdir = os.path.dirname(os.path.abspath(__file__))
+    file = parentdir + os.sep + 'automation_config.yaml'
+    key = sys.argv[1]
+    result = get_value(file, key)
+    print(result)
--- scripts/automation/update_ssp.sh
+++ scripts/automation/update_ssp.sh
@@ -4,7 +4,7 @@ export COMMIT_TITLE="chore: Component definitions automatic update."
 export COMMIT_BODY="Sync components with ocp-oscal-ssp repo"
 git config --global user.email "automation@example.com"
 git config --global user.name "AutomationBot" 
-cd e2e-demo-ssp
+cd $(python ./scripts/automation/automation_config.py "update-ssp-folder")
 git checkout -b "cd_autoupdate_$GITHUB_RUN_ID"
 cp -r ../catalogs .
 cp -r ../profiles . 
@@ -20,7 +20,7 @@ else
      echo "Nothing to commit" 
   else
      git commit -m "$COMMIT_TITLE"
-     remote=https://$GH_TOKEN@github.com/oscal-compass/e2e-demo-ssp
+     remote=$(python ../scripts/automation/automation_config.py "update-ssp-remote")
      git push -u "$remote" "cd_autoupdate_$GITHUB_RUN_ID"
      echo $COMMIT_BODY
      gh pr create -t "$COMMIT_TITLE" -b "$COMMIT_BODY" -B "develop" -H "cd_autoupdate_$GITHUB_RUN_ID" 
